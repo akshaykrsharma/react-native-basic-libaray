75b48541b90e6760713efa10330711361a3be6a4
Akshay Sharma <akshay@punchh.com>
2018-10-29 17:33:06 +0530

    Implemented Matrial UI for login

--- BasicLibrary/src/Redux/CounterApp.js
+++ BasicLibrary/src/Redux/CounterApp.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
 import Button from '../Components/Button';
 import Utils from '../Components/Utils';
+import { TextField } from 'react-native-material-textfield';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import InputTextLayout from '../Components/InputTextLayout';
+
 class CounterApp extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			errorMsg: null
+		};
 	}
+
 	// state = {
 	// 	counter: 0
 	// };
@@ -19,45 +27,71 @@ class CounterApp extends Component {
 	// 	this.setState({ counter: this.state.counter - 1 });
 	// };
 
-	onPress() {
-		console.warn('btn is Clicked');
+	onPress = () => {
+		console.warn('btn is Clicked ' + this.state.password);
+		const errorMsg = 'please enter a valid password';
+		this.setState({ errorMsg });
+	};
+
+	onAccessoryPress() {
+		console.warn('asdfasdf');
+	}
+
+	renderPasswordAccessory() {
+		let name = true ? 'visibility' : 'visibility-off';
+
+		return (
+			<MaterialIcon
+				size={24}
+				name={name}
+				color={TextField.defaultProps.baseColor}
+				onPress={this.onAccessoryPress}
+				suppressHighlighting
+			/>
+		);
 	}
 
 	render() {
 		return (
-			<View>
-				<View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-					<TouchableOpacity onPress={() => this.props.increaseCounter()}>
-						<Text style={{ fontSize: 20, padding: 10 }}>INCREASE</Text>
-					</TouchableOpacity>
-					<Text style={{ fontSize: 20, padding: 10 }}>{this.props.counter}</Text>
-					<TouchableOpacity onPress={() => this.props.decreaseCounter()}>
-						<Text style={{ fontSize: 20, padding: 10 }}>DECREASE</Text>
-					</TouchableOpacity>
+			<ScrollView>
+				<View>
+					<View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+						<TouchableOpacity onPress={() => this.props.increaseCounter()}>
+							<Text style={{ fontSize: 20, padding: 10 }}>INCREASE</Text>
+						</TouchableOpacity>
+						<Text style={{ fontSize: 20, padding: 10 }}>{this.props.counter}</Text>
+						<TouchableOpacity onPress={() => this.props.decreaseCounter()}>
+							<Text style={{ fontSize: 20, padding: 10 }}>DECREASE</Text>
+						</TouchableOpacity>
+					</View>
+
+					<InputTextLayout
+						label="User Name"
+						title="It will be your login id"
+						onChangeText={username => {
+							this.setState({ username });
+						}}
+					/>
+					<InputTextLayout
+						isPassword={true}
+						label="Password"
+						title="make it hard to crack"
+						onChangeText={password => {
+							this.setState({ password });
+						}}
+					/>
+
+					<Button
+						title={'SUBMIT'}
+						onPress={this.onPress}
+						containerStyle={{ marginTop: 30, width: '80%' }}
+						myStyle={{ color: 'white' }}
+					/>
 				</View>
-				<Button title={'SUBMIT'} onPress={this.onPress} containerStyle={{ backgroundColor: 'blue' }} myStyle={{color:'yellow'}} />
-				<Button title={'UPDATE'} onPress={this.onPress} />
-			</View>
+			</ScrollView>
 		);
 	}
 }
-
-const styles = StyleSheet.create({
-	buttonStyle: {
-		minWidth: 200,
-		minHeight: 30,
-		//shadowOffset: { width: 0, height: 5 },
-		backgroundColor: '#00b0ff'
-	},
-	buttonTitleStyle: {
-		color: 'rgba(255,255,255,1)',
-		minHeight: 60
-	},
-	c: {
-		minHeight: 40,
-		backgroundColor: 'red'
-	}
-});
 
 function mapStateToProps(state) {
 	return {
